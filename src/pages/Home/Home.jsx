f046f733d08951f94cb6ef0f0b920528fe8cec65
Saim <admin@gmail.com>
2024-05-09 00:34:07 +0600

    fetch all the tourist data from database

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,30 +3,56 @@ import Banner from './Banner/Banner'
 import TouristCard from '../../components/TouristCard'
 import PricingSection from './Pricing/PricingSection'
 import DestinationSection from './PopularDestination/DestinationSection'
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { AuthContext } from '../../providers/AuthProvider'
+import axios from 'axios'
  
 
- 
 
 
 const Home = () => {
   const {user} = useContext(AuthContext);
+  const [touristSpots, setTouristSpots] = useState([]);
+  const [loading,setLoading] = useState(true);
+
+
+  const fetchTouristSpots = async () => {
+    try {
+        const response = await axios.get('http://localhost:3000/tourist_spots');
+        setTouristSpots(response.data);
+        setLoading(false);
+    } catch (error) {
+        console.error('Error fetching tourist spots:', error);
+    }
+};
+
+  useEffect(() => {
+      fetchTouristSpots();
+  }, []);
+  console.log(touristSpots);
    
   return (
      <>
         <Banner/>
         <div class="container mx-auto py-8">
             <h2 class="text-3xl font-bold mb-[4rem] text-center">Popular Tourist Spots</h2>
-            <div className="container mx-auto  grid grid-cols-1 md:grid-cols-3 gap-4">
-                <TouristCard/>
-                <TouristCard/>
-                <TouristCard/>
-                <TouristCard/>
-                <TouristCard/>
-                <TouristCard/>
+        
+              {loading ? 
+                      <div className="w-full flex items-center justify-center">
+                        <span className="loading loading-bars loading-lg"></span>
+                     </div>
+                :
+               (
+                 <div className="container mx-auto  grid grid-cols-1 md:grid-cols-3 gap-4">
+                       {touristSpots.map(touristSpot=> <TouristCard touristSpot={touristSpot}/>)}
+                 </div> 
+                 )
+              }
+              
    
-            </div>
+ 
+   
+            
             <PricingSection/>
             <DestinationSection/>
 
